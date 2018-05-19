3f5738e9e13c39a711ad55ac44cea07f032c764c
Sharon <sbousso.de@mysummitps.org>
2018-05-19 03:17:42 +0000

    Minimally viable Product! (client)

--- server3.js
+++ server3.js
@@ -1,18 +1,28 @@
-
 var io = require('socket.io-client');
 var socket = require('socket.io-client')('http://aichallenge-gorgamite.c9users.io');
 
-var name = "hellaow";
+var name = "helglaow";
 socket.emit("name", name);
 
-socket.on("update", function(game){
-    console.log(game.idTurn + "   " + game.players)
-if(game.players.name[game.players.length-1] == name){
-    console.log(game.names[game.players.length-1] + " is " + name);
-}
+
+
+socket.on("update", function(game) {
+    // console.log(game.idTurn + "   " + game.players.length)
+    if (game.players[game.idTurn].name == name) {
+        //EVALUATEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEeee
+        socket.emit("new direction", {dir: direction(), name: name});
+    }
 })
-socket.on("queue", function(str){
+socket.on("queue", function(str) {
     console.log(str);
 })
+
+function direction(game) {
+    // console.log(game.players[game.idTurn])
+    // game.players[game.idTurn].dir = "west";
+    return "west";
+}
+
+// hi
 
 // socket.on("turn", function())
